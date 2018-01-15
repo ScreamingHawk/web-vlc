e80fa7759155ca36ecb97116ddc032acb4ad68c7
Michael Standen <screaminghawk@gmail.com>
2018-01-16 10:38:24 +1300

    Add show details

--- client/src/components/ShowList.jsx
+++ client/src/components/ShowList.jsx
@@ -2,32 +2,56 @@ import React, { Component } from 'react';
 
 export default class ShowList extends Component {
 	constructor(props){
-		super(props);
-		this.state = {shows: []};
+		super(props)
+		this.state = {shows: []}
 	}
 	componentDidMount(){
-		this.getShowList();
+		this.getShowList()
 	}
-	getShowList(){
-		fetch('/shows')
-			.then(results => {
-				return results.json();
-			}).then(data => {
-				this.setState({shows: data});
-			});
+	async getShowList(){
+		const shows = await (await fetch('/shows')).json()
+		this.setState({shows: shows})
+
+		const apiData = await (await fetch('/config/api/omdb')).json()
+		for (let i in shows){
+			const details = await (await fetch(`${apiData.url}?apikey=${apiData.key}&t=${shows[i].name}`)).json()
+			if (details.Response == "True"){
+				const newShows = this.state.shows.slice()
+				let show = newShows[i]
+				show.image = details.Poster
+				show.plot = details.Plot
+				show.imdbRating = details.imdbRating
+				this.setState({shows: newShows})
+			}
+		}
 	}
 	render() {
 		let showRenders = this.state.shows.map(function(show){
+			//TODO Placeholder image
+			let imgSrc = null
+			if (show.image){
+				imgSrc = show.image
+			}
 			return (
-				<div>
-					<h3>{show.name}</h3>
-					<p>Seasons: {show.seasons}</p>
-					<p>Episodes: {show.count}</p>
+				<div className="box card center" key={show.name}>
+					<img src={imgSrc}></img>
+					<div className="content">
+						<h3>{show.name}</h3>
+						<p>{show.plot}</p>
+						<p>
+							<b>IMDB Rating:</b> {show.imdbRating}
+						</p>
+						<p>
+							<b>Seasons on disk:</b> {show.seasons.length > 0 ? show.seasons.join(", ") : "None"}
+							<br/>
+							<b>Episodes on disk:</b> {show.count}
+						</p>
+					</div>
 				</div>
 			)
 		});
 		return (
-			<div>
+			<div className="flex">
 				{showRenders}
 			</div>
 		)
