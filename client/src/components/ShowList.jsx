2b34e79593965770818d3721540fcc6f52cbc3b7
Michael Standen <screaminghawk@gmail.com>
2018-01-21 20:43:58 +1300

    Add show filtering

--- client/src/components/ShowList.jsx
+++ client/src/components/ShowList.jsx
@@ -4,25 +4,51 @@ import Show from './Show.jsx'
 export default class ShowList extends Component {
 	constructor(props){
 		super(props)
-		this.state = {shows: []}
+		this.state = {
+			shows: [],
+			filteredShows: [],
+			search: ""
+		}
+
+		this.filterShows = this.filterShows.bind(this)
 	}
 	componentDidMount(){
 		this.getShowList()
 	}
 	async getShowList(){
 		const shows = await (await fetch('/shows')).json()
-		this.setState({shows: shows})
+		this.setState({
+			shows: shows,
+			filteredShows: shows,
+			search: ""
+		})
+	}
+	filterShows(event){
+		let search = event.target.value
+		let shows = this.state.shows
+		let filteredShows = shows.filter(show => show.name.toLowerCase().indexOf(search.toLowerCase()) > -1)
+
+		this.setState({
+			shows: shows,
+			filteredShows: filteredShows,
+			search: search
+		})
 	}
 	render() {
 		const setVideo = this.props.setVideo
-		let showRenders = this.state.shows.map(function(show){
+		let showRenders = this.state.filteredShows.map(function(show){
 			return (
 				<Show {...show} key={show.name} setVideo={setVideo} />
 			)
 		});
 		return (
-			<div className="flex wrap justify">
-				{showRenders}
+			<div>
+				<div className="flex">
+					<input placeholder="Search..." value={this.state.search} onChange={this.filterShows}></input>
+				</div>
+				<div className="flex wrap justify">
+					{showRenders}
+				</div>
 			</div>
 		)
 	}
